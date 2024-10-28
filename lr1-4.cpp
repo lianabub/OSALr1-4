40b3313452cceb9a7e49875292f46a243e390e7d
liana <lianabub07@gmail.com>
2024-10-28 12:39:16 +0300

    Add files via upload

---
+++ lr1-4.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    double z1, z2, b, a;
+    std::cout << "Введите a \n";
+    std::cin >> a;
+    std::cout << "Введите b \n";
+    std::cin >> b;
+    z1 = (sin(a) + cos((2 * b - a))) / (cos(a) - sin(2 * b - a));
+    z2 = (1 + sin(2 * b)) / (cos(2 * b));
+    std::cout << " z1 =" << z1 << " ; z2 = " << z2;
+    return 0;
+}
